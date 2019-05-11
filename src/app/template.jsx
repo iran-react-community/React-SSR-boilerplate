c3efc1e0ef5feaf205d02f930f0be7a02dc9cdd3
nimahkh <nima.2004hkh@gmail.com>
2019-05-12 00:30:55 +0430

    add sheets for rendering jsx

--- src/app/template.jsx
+++ src/app/template.jsx
@@ -1,10 +1,11 @@
-export default ({ markup, helmet }) => {
-   return `<!DOCTYPE html>
+export default ({markup, helmet,sheets}) => {
+    return `<!DOCTYPE html>
          <html ${helmet.htmlAttributes.toString()}>
             <head>
                ${helmet.title.toString()}
                ${helmet.meta.toString()}
-               ${helmet.link.toString()}              
+               ${helmet.link.toString()}      
+               <style type="text/css">${sheets.toString()}</style>        
             </head>
             <body ${helmet.bodyAttributes.toString()}>
                <div id="root">${markup}</div>

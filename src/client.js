a71b323d5a24eb6a31c684da8daad7c48d1a6c8f
fatemeh <fatemehsharafi7394@gmail.com>
2019-06-08 01:12:38 +0430

    add redux

--- src/client.js
+++ src/client.js
@@ -2,9 +2,20 @@ import React from 'react';
 import {hydrate} from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import App from './app/App';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import counterApp from './reducers'
+
+const preloadedState = window.__PRELOADED_STATE__
+
+delete window.__PRELOADED_STATE__
+
+const store = createStore(counterApp, preloadedState)
 
 hydrate((
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
 ), document.getElementById('root'));
